b4ea84ea7f75bc555d5652fd6e6ef7b29121d2b9
Nicolas Treiber <nicolas.treiber@gmail.com>
2023-01-26 21:33:59 +0000

    Added scene loader component

---
+++ Assets/Scripts/Game/SceneManagement/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utility;
+
+namespace Game.SceneManagement {
+	public class SceneLoader : MonoBehaviour {
+		[SerializeField, Scene] private string scene;
+
+		public void Load() {
+			Debug.Assert(!string.IsNullOrWhiteSpace(scene), $"Scene loader {name} is missing scene");
+
+			SceneManager.LoadScene(scene);
+		}
+	}
+}
