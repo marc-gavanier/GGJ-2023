a2d5906f6f7ece4cef6392a8dd302810826b455e
Nicolas Treiber <nicolas.treiber@gmail.com>
2023-01-28 07:48:08 +0000

    Added language management

---
+++ Assets/Scripts/Config/Configuration.cs
@@ -0,0 +1,22 @@
+﻿using DependencyInjection.SharedValues;
+using UnityEditor;
+using UnityEngine;
+using Utility;
+
+namespace Config {
+	public class Configuration : ScriptableObject {
+		[SerializeReference] private StringValue language;
+
+		[MenuItem("Assets/Create/GGJ/Configuration", false)]
+		private static void Create() {
+			var configuration = ScriptableObject.CreateInstance<Configuration>();
+			configuration.SaveAsset("Configuration");
+			configuration.language = configuration.AddString("Language");
+		}
+
+		[MenuItem("Assets/Create/GGJ/Configuration", true)]
+		private static bool ValidateCreate() {
+			return SelectionUtility.IsFolder;
+		}
+	}
+}
