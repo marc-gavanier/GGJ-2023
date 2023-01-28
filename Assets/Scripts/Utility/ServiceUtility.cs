d69883c60aa51dcf15d7f124993689a32e5f4b50
Nicolas Treiber <nicolas.treiber@gmail.com>
2023-01-28 07:37:39 +0000

    Some utility classes

---
+++ Assets/Scripts/Utility/ServiceUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Utility {
+	public static class ServiceUtility {
+		public static TService Find<TService>() where TService : ScriptableObject {
+			var serviceType = typeof(TService);
+			var guids = AssetDatabase.FindAssets($"t:{serviceType.Name}");
+			Debug.Assert(guids.Length > 0, $"This is no {serviceType.Name}");
+			Debug.Assert(guids.Length == 1, $"There is multiple {serviceType.Name}");
+
+			var path = AssetDatabase.GUIDToAssetPath(guids[0]);
+
+			return AssetDatabase.LoadAssetAtPath(path, serviceType) as TService;
+		}
+	}
+}
---
+++ Assets/Scripts/Utility/ShowReferenceAttribute.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+namespace Utility {
+	public class ShowReferenceAttribute : PropertyAttribute {
+		
+	}
+}
