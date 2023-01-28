d4c4acff27e725c62f72d2b013d7ba3ec65909fe
Nicolas Treiber <nicolas.treiber@gmail.com>
2023-01-28 13:31:32 +0000

    Settings now use shared values

--- Assets/Scripts/UI/Menu/SettingsMenu.cs
+++ Assets/Scripts/UI/Menu/SettingsMenu.cs
@@ -5,16 +5,6 @@ namespace UI.Menu
 {
     public class SettingsMenu : MonoBehaviour
     {
-        public AudioMixer audioMixer;
-
-        private void ApplyVolumeTo(string groupName, float volume) => audioMixer.SetFloat(groupName, volume <= -40 ? -80 : volume);
-
-        public void SetMasterVolume(float volume) => ApplyVolumeTo("MasterVolume", volume);
-
-        public void SetSoundVolume(float volume) => ApplyVolumeTo("SoundVolume", volume);
-
-        public void SetMusicVolume(float volume) => ApplyVolumeTo("MusicVolume", volume);
-
         public void SetFullScreen(bool isFullScreen) => Screen.fullScreen = isFullScreen;
     }
 }
