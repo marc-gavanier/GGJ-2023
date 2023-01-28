804557472e9a43714477b065e0ca802a60573427
Nicolas Treiber <nicolas.treiber@gmail.com>
2023-01-28 10:51:17 +0000

    Added slider binder

---
+++ Assets/Scripts/UI/ValueBinders/SliderBinder.cs
@@ -0,0 +1,23 @@
+﻿using DependencyInjection.SharedValues;
+using UnityEngine;
+using UnityEngine.UI;
+using Utility;
+
+namespace UI.ValueBinders {
+	[DisallowMultipleComponent]
+	public class SliderBinder : MonoBehaviour {
+		[SerializeField, ShowReference] private FloatValue value;
+
+		private Slider slider;
+
+		private void Awake() {
+			slider = GetComponentInChildren<Slider>();
+			slider.value = value;
+			slider.onValueChanged.AddListener(UpdateValue);
+		}
+
+		private void UpdateValue(float newValue) {
+			value.Value = newValue;
+		}
+	}
+}
