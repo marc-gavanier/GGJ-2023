a8437ffa317d5d49568c7fa2798c3853184f5ce5
Nicolas Treiber <nicolas.treiber@gmail.com>
2023-01-28 15:16:33 +0000

    Fixed service finding

--- Assets/Scripts/Utility/ServiceUtility.cs
+++ Assets/Scripts/Utility/ServiceUtility.cs
@@ -4,10 +4,8 @@ namespace Utility {
 	public static class ServiceUtility {
 		public static TService Find<TService>() where TService : ScriptableObject {
 			var instances = Resources.FindObjectsOfTypeAll<TService>();
-			Debug.Assert(instances.Length > 0, $"This is no {(typeof(TService)).Name}");
-			Debug.Assert(instances.Length == 1, $"There is multiple {(typeof(TService)).Name}");
 			
-			return instances[0];
+			return instances.Length == 0 ? null : instances[0];
 		}
 	}
 }
