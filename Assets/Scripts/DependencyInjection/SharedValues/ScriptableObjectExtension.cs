db074a3dcd220b389ce11858dccd6c760a3639cb
Nicolas Treiber <nicolas.treiber@gmail.com>
2023-01-28 07:35:42 +0000

    Extended scriptable objects to handle nested assets creation

---
+++ Assets/Scripts/DependencyInjection/SharedValues/ScriptableObjectExtension.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace DependencyInjection.SharedValues {
+	public static partial class ScriptableObjectExtension {
+		public static TValue AddValue<TValue>(this ScriptableObject target, string name) where TValue : ScriptableObject {
+			var value = ScriptableObject.CreateInstance<TValue>();
+			value.name = name;
+			AssetDatabase.AddObjectToAsset(value, target);
+			AssetDatabase.SaveAssets();
+			AssetDatabase.Refresh();
+
+			return value;
+		}
+	}
+}
