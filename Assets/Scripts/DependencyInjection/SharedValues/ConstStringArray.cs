4781c0b16726461d53f85bff983046286948f89f
Nicolas Treiber <nicolas.treiber@gmail.com>
2023-01-28 07:30:14 +0000

    Added shared string & string array

---
+++ Assets/Scripts/DependencyInjection/SharedValues/ConstStringArray.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace DependencyInjection.SharedValues {
+	[CreateAssetMenu(menuName = "GGJ/Values/Const String Array")]
+	public class ConstStringArray : BaseConstArray<string> {
+		
+	}
+	
+	public static partial class ScriptableObjectExtension {
+		public static ConstStringArray AddConstStringArray(this ScriptableObject target, string name) => target.AddValue<ConstStringArray>(name);
+	}
+}
